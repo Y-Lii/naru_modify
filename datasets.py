e0df1a4109ee9ee71ecc005e25ca1057b4c6c082
Y-Lii <56451662+Y-Lii@users.noreply.github.com>
2021-10-04 12:16:33 +0800

    From naru

---
+++ datasets.py
@@ -0,0 +1,18 @@
+"""Dataset registrations."""
+import os
+
+import numpy as np
+
+import common
+
+
+def LoadDmv(filename='Vehicle__Snowmobile__and_Boat_Registrations'):
+    csv_file = './datasets/{}.csv'.format(filename)
+    cols = [
+        'id', 'movie_id', 'linked_movie_id', 'link_type_id'
+    ]
+    # Note: other columns are converted to objects/strings automatically.  We
+    # don't need to specify a type-cast for those because the desired order
+    # there is the same as the default str-ordering (lexicographical).
+    type_casts = {'birth_date': np.datetime64, 'death_date': np.datetime64, 'release dates': np.datetime64}
+    return common.CsvTable(filename, csv_file, None, type_casts)
